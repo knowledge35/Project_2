795d51c74ea28ccd1941aeac816bcec5782cb21a
scientiest35 <ibomyakmc@hotmasdail.com>
2018-12-08 23:46:45 +0300

    update

--- Project_2/Program.cs
+++ Project_2/Program.cs
@@ -10,6 +10,16 @@ namespace Project_2
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i <=5; i++)
+            {
+                for (int j = 1; j <=5; j++)
+                {
+                    Console.Write(i);
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey(true);
         }
     }
 }
